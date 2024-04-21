2d66590a49aa4feb4e888fd9ba7e1f5090f607b2
Ítalo <lixoliteral@gmail.com>
2024-04-21 20:30:24 -0300

    Refatora: Simplifica o código

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
+import React from "react";
 import { Header } from "./components/Header";
 import { LinkList, LinkButton } from "./components/LinkList";
-// Ícones
-import { FaGithub } from "react-icons/fa6";
-import { FaInstagram } from "react-icons/fa6";
-import { FaLinkedinIn } from "react-icons/fa6";
-import { FaMastodon } from "react-icons/fa6";
-
+import { FaGithub, FaInstagram, FaLinkedinIn, FaMastodon } from "react-icons/fa6";
 import { Dialog, Trigger, Content } from "./components/Dialog";
 import "./components/Dialog.css";
-
 import { coollist, profile, footerAtr } from "../info";
 
 export function App() {
@@ -17,27 +12,20 @@ export function App() {
             <Header />
             <LinkList>
                 {coollist.map((link, i) => {
-                    if (link.type) {
-                        return (
-                            <Dialog type={link.type} key={i}>
-                                <Trigger>
-                                    <LinkButton
-                                        href={link.link}
-                                        children={link.title}
-                                    />
-                                </Trigger>
-                                <Content>{link.content}</Content>
-                            </Dialog>
-                        );
-                    } else {
-                        return (
-                            <LinkButton
-                                key={i}
-                                href={link.link}
-                                children={link.title}
-                            />
-                        );
-                    }
+                    return link.type ? (
+                        <Dialog type={link.type} key={i}>
+                            <Trigger>
+                                <LinkButton href={link.link}>
+                                    {link.title}
+                                </LinkButton>
+                            </Trigger>
+                            <Content>{link.content}</Content>
+                        </Dialog>
+                    ) : (
+                        <LinkButton key={i} href={link.link}>
+                            {link.title}
+                        </LinkButton>
+                    );
                 })}
             </LinkList>
             <LinkList medialist>
@@ -56,14 +44,12 @@ export function App() {
             </LinkList>
             <footer>
                 Feito com ❤️ por{" "}
-                {
-                    <Dialog type="modal">
-                        <Trigger>
-                            <a href="#">@{profile.usernames.join("@")}</a>
-                        </Trigger>
-                        <Content>{footerAtr}</Content>
-                    </Dialog>
-                }
+                <Dialog type="modal">
+                    <Trigger>
+                        <a href="#">@{profile.usernames.join("@")}</a>
+                    </Trigger>
+                    <Content>{footerAtr}</Content>
+                </Dialog>
             </footer>
         </>
     );
