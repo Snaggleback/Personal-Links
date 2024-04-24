8e7aacbbb0cabdb33f619882be66cc11f9b35e96
Ítalo <lixoliteral@gmail.com>
2024-04-24 19:53:06 -0300

    Melhoria: Arquivo para referência para a pasta
    Arquivo onde ficam as referências dos componentes

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+// components/index.js
+
+export { Header } from "./Header";
+export { LinkList } from "./LinkList";
+export { LinkButton } from "./LinkList";
+export { Dialog } from "./Dialog";
+export { Trigger } from "./Dialog";
+export { Content } from "./Dialog";
+export { NormalLink } from "./NormalLink";
+export { BigTitle } from "./BigTitle";
